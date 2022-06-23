8ad9c7066ffb54488aa2d5fb2f3ab0c2242a4070
hamogor <orefdev@gmail.com>
2022-06-23 20:18:22 +0100

    first commit

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/template/html"
+)
+
+func main() {
+	engine := html.New("./views", ".html")
+
+	app := fiber.New(fiber.Config{
+		Views: engine,
+	})
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.Render("index", fiber.Map{
+			"Title": "Hello, World!",
+		})
+	})
+
+	app.Static("/posts", "./posts")
+
+	app.Listen(":3000")
+}
